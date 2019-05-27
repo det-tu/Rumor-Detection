6378daecc2fc9d445129482ed628fb120e11d041
det-tu <635509361@qq.com>
2019-05-27 11:07:45 +0800

    update

--- code/extractword.py
+++ code/extractword.py
@@ -1,1 +1,1 @@
-import jiebaimport jieba.posseg as psegs = "我爱中国"l = jieba._lcut(s)print(l)words = pseg.cut(s)result = []for w in words:    result.append(str(w.word)+"/"+str(w.flag))print(result)
+import jiebaimport jieba.posseg as psegfrom gensim.models import Word2Vecs = "我爱中国"l = jieba._lcut(s)print(l)words = pseg.cut(s)result = []for w in words:    result.append(str(w.word)+"/"+str(w.flag))print(result)
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,11 +1,23 @@
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import torch.optim as optim
-import torch.nn.functional as F
-import nltk
-import random
-import numpy as np
-from collections import Counter
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction import text
+from sklearn import decomposition, ensemble
+from sklearn.naive_bayes import MultinomialNB
+from keras import layers, models, optimizers
+from gensim.models import Word2Vec
 
-print(torch.__version__)
+import pandas as pd
+import numpy as np
+import string, jieba, re, os, sys
+import logging
+
+# Extract content of truth
+
+
+
+# 多线程分词
+jieba.enable_parallel()
+
+# 停用词
+def getStopwords():
+    stopwords = []
